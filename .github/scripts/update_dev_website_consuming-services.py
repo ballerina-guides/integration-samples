d6dc1c7d38e167fb23f39f67a9b802d748c0fffd
Tharik Kanaka <tharik.kanaka@gmail.com>
2023-03-13 11:26:56 +0530

    Include workflows

---
+++ .github/scripts/update_dev_website_consuming-services.py
@@ -0,0 +1,9 @@
+from common_utils import update_md
+
+
+def main():
+    update_md('consuming_services/consuming_services.bal', 'consuming-services.md',
+              'update-consuming-services', 'Consuming Services')
+
+
+main()
