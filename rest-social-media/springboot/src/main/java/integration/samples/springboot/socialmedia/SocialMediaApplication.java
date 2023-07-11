066942e57d7f911370cfa6b7c1d1cafb5d4c30b3
ayeshLK <ayesh9303@gmail.com>
2023-07-11 12:37:50 +0530

    Add missing config annotation

--- rest-social-media/springboot/src/main/java/integration/samples/springboot/socialmedia/SocialMediaApplication.java
+++ rest-social-media/springboot/src/main/java/integration/samples/springboot/socialmedia/SocialMediaApplication.java
@@ -2,8 +2,10 @@ package integration.samples.springboot.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
