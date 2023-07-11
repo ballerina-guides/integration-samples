b3352a418ff799fcf0876c36005908aef9229831
ayeshLK <ayesh9303@gmail.com>
2023-07-11 13:57:10 +0530

    Add retry config

--- rest-social-media/springboot-reactive/src/main/java/integration/samples/springboot/reactive/socialmedia/sentiment/SentimentAnalysisClientImpl.java
+++ rest-social-media/springboot-reactive/src/main/java/integration/samples/springboot/reactive/socialmedia/sentiment/SentimentAnalysisClientImpl.java
@@ -1,15 +1,24 @@
 package integration.samples.springboot.reactive.socialmedia.sentiment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Service
 public class SentimentAnalysisClientImpl implements SentimentAnalysisClient {
     private final WebClient sentimentClient;
+
+    @Value("${sentiment-api.maxRetryAttempts}")
+    private int maxRetryAttempts;
+    @Value("${sentiment-api.waitDuration}")
+    private int waitDuration;
 
     @Autowired
     public SentimentAnalysisClientImpl() {
@@ -23,6 +32,7 @@ public class SentimentAnalysisClientImpl implements SentimentAnalysisClient {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromValue(content))
                 .retrieve()
-                .bodyToMono(Sentiment.class);
+                .bodyToMono(Sentiment.class)
+                .retryWhen(Retry.fixedDelay(maxRetryAttempts, Duration.ofSeconds(waitDuration)));
     }
 }
