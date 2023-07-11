d52aff6110458c0b18571cf36b95061edd2d47e9
ayeshLK <ayesh9303@gmail.com>
2023-07-11 12:15:50 +0530

    Refactor the MySQL init script

--- rest-social-media/resources/db/init.sql
+++ rest-social-media/resources/db/init.sql
@@ -1,31 +1,31 @@
-CREATE TABLE users (
+CREATE TABLE user (
     id INT NOT NULL auto_increment PRIMARY KEY,
     birth_date DATE,
     name VARCHAR(255)
 );
-CREATE TABLE posts (
+CREATE TABLE post (
     id INT NOT NULL auto_increment PRIMARY KEY,
     description VARCHAR(255),
     user_id INT
 );
 
-insert into users(birth_date, name)
+insert into user(birth_date, name)
 values(current_date(), 'Ranga');
 
-insert into users(birth_date, name)
+insert into user(birth_date, name)
 values(current_date(), 'Ravi');
 
-insert into users(birth_date, name)
+insert into user(birth_date, name)
 values(current_date(), 'Sathish');
 
-insert into posts(description, user_id)
+insert into post(description, user_id)
 values('I want to learn AWS', 1);
 
-insert into posts(description, user_id)
+insert into post(description, user_id)
 values('I want to learn DevOps', 1);
 
-insert into posts(description, user_id)
+insert into post(description, user_id)
 values('I want to learn GCP', 2);
 
-insert into posts(description, user_id)
+insert into post(description, user_id)
 values('I want to learn multi cloud', 3);
