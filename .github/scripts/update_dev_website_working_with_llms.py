f0b8688c492fb4aa8fe1b5d12302830292ca8e85
MaryamZi <maryamziyadm@gmail.com>
2025-04-28 18:39:01 +0530

    Update scripts and workflows

---
+++ .github/scripts/update_dev_website_working_with_llms.py
@@ -0,0 +1,9 @@
+from common_utils import update_md
+
+
+def main():
+    update_md('working_with_llms/working_with_llms.bal', 'working-with-llms.md',
+              'update-working-with-llms', 'Working with LLMs')
+
+
+main()
